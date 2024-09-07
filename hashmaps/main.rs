585b1e92a76c56f9e272691f26b3e419f1ff1e4b
Quazar <112936536+realquazar@users.noreply.github.com>
2024-09-07 12:23:12 +0530

    Update main.rs

--- hashmaps/main.rs
+++ hashmaps/main.rs
@@ -15,21 +15,22 @@ use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 fn main() {
-  println!("Enter string:")
-  let input = io::stdin();
-  let mut map: HashMap<String, u32> = HashMap::new();
+    println!("Enter string:")
+    let input = io::stdin();
+    let mut map: HashMap<String, u32> = HashMap::new();
 
-  for line in input.lock().lines() {
-    let line = line.expect("Failed to read line");
-    let words = Vec<&str> = line.split_whitespace().collect();
+    for line in input.lock().lines() {
+        let line = line.expect("Failed to read line");
+        let words = Vec<&str> = line.split_whitespace().collect();
 
-    for word in words {
-      let count = map.entry(word.to_string()).or_insert(0);
-      *count += 1;
+        for word in words {
+            let count = map.entry(word.to_string()).or_insert(0);
+            *count += 1;
+        }
     }
-  }
-  println("Word Frequencies");
-  for (word, count) in map {
-    println!("{}: {}", word, count);
+    
+    println("Word Frequencies");
+    for (word, count) in map {
+        println!("{}: {}", word, count);
   }
 }
