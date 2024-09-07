73b32b13d970fd90e790e074ad38d39970275e2f
Quazar <112936536+realquazar@users.noreply.github.com>
2024-09-07 12:16:01 +0530

    Create easy_input.rs

---
+++ user_input/easy_input.rs
@@ -0,0 +1,25 @@
+/*
+This is a shorter way to read the string given by the userand perform the desired action.
+This method uses an external package called text_io which has a property called "read", this will help us to shorten out code
+*/
+
+use text_io::read;
+
+fn main() {
+    
+    // Float input
+    println!("Enter a number (f64): ");
+    let float_num: f64 = read!();
+  
+    // Integer input
+    println!("Enter an integer (i32): ");
+    let int_num: i32 = read!();
+    
+    // String input
+    println!("Enter a string: ");
+    let str_input: String = read!();
+
+    println!("You entered a number: {}", float_num);
+    println!("You entered an integer: {}", int_num);
+    println!("You entered a string: {}", str_input);
+}
